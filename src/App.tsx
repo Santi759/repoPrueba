0758b892ad61c2b55c2c712434264a73a7590260
Santiago <santichepilla18@gmail.com>
2024-10-13 20:19:25 -0300

    contador terminado

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import { useState } from "react"
 
 function App() {
 
 
+  const [count, setCount] = useState(0)
+
+
   return (
     <div>
-
+      <button onClick ={()=>{setCount((prev)=> prev+1)}}>Incrementar</button>
+      <p>{count}</p>
+      <button onClick ={()=>{setCount((prev)=> prev-1)}}>Decrementar</button>
     </div>
   )
 }
